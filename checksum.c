352b4899baf6a2f0942848595a299003fe02a751
Deni Cabaravdic <37716587+d3h1@users.noreply.github.com>
2021-02-16 20:10:35 -0800

    Add files via upload

---
+++ checksum.c
@@ -0,0 +1,59 @@
+/********************************/
+/* Program Name: Checksum.c     */
+/* Author: Deni Cabaravdic      */
+/* Date:   02/15/2021           */
+/********************************/
+/* Description: To write the same checksum program we diud in java but in C with a twist*/
+/* Validation Checks:           */
+/* Enhancements:                */
+/********************************/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <sys/types.h>
+#include <unistd.h>
+​
+#define max_int (255)
+#define byte unsigned char
+​
+int main (int argc, char * argv[], char ** envp) {
+​
+  int count = 10;
+  int sum = 0;
+  int quotient;
+  int remainder;
+  byte checksum;
+  byte complement;
+
+  byte header[10];
+​
+  read(STDIN_FILENO, &header, 10);
+​
+  for (int x = 0; x < count; x++) {
+    printf("%d ", header[x]);
+    printf("\n");
+  }
+​
+  for (int x = 0; x < count; x++) {
+    if (x == 5) {
+      checksum = header[x];
+      header[x] = 0;
+    }
+    sum += header[x];
+  }
+​
+    quotient = sum / (max_int + 1);
+    remainder = sum % (max_int + 1);
+    sum = quotient + remainder;
+    complement = max_int - sum;
+​
+  /* the following is the prototype for the read system call */
+  /* int read(int fildes, void *buf, size_t nbyte);  */
+​
+fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+  if (checksum != complement ) {
+    fprintf(stderr, "Error Detected!\n"); 
+    return 1;
+  }
+  return 0;
+}
