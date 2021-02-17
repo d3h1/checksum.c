2a1694ceda56b6283f027b82e336f840e3a05a00
Deni Cabaravdic <37716587+d3h1@users.noreply.github.com>
2021-02-16 20:15:50 -0800

    Add files via upload

--- checksum.c
+++ checksum.c
@@ -10,48 +10,49 @@
 
 #include "stdio.h"
 #include "stdlib.h"
-#include <sys/types.h>
 #include <unistd.h>
-​
+
 #define max_int (255)
 #define byte unsigned char
-​
-int main (int argc, char * argv[], char ** envp) {
-​
-  int count = 10;
-  int sum = 0;
+
+ int main (int argc, char * argv[], char ** envp) 
+ {
+
+  int sum = 0;   
+  byte checksum; 
+  byte complement;
   int quotient;
   int remainder;
-  byte checksum;
-  byte complement;
 
   byte header[10];
-​
+
+  
+
   read(STDIN_FILENO, &header, 10);
-​
-  for (int x = 0; x < count; x++) {
-    printf("%d ", header[x]);
-    printf("\n");
-  }
-​
-  for (int x = 0; x < count; x++) {
-    if (x == 5) {
-      checksum = header[x];
-      header[x] = 0;
-    }
-    sum += header[x];
-  }
-​
-    quotient = sum / (max_int + 1);
-    remainder = sum % (max_int + 1);
-    sum = quotient + remainder;
-    complement = max_int - sum;
-​
-  /* the following is the prototype for the read system call */
-  /* int read(int fildes, void *buf, size_t nbyte);  */
-​
-fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
-  if (checksum != complement ) {
+
+      for (int c = 1; c <= count ; c ++ )
+      {
+              if (c == 6) 
+              {
+                checksum = header[c];
+                header[c] = 0;
+              }
+
+              sum += header[c];
+      }
+
+      quotient   = sum / (max_int + 1);
+      remainder  = sum % (max_int + 1);
+      sum = quotient + remainder;
+      complement = max_int - sum;
+
+/* the following is the prototype for the read system call */
+/* int read(int fildes, void *buf, size_t nbyte);  */
+
+
+  fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+  if (checksum != complement ) 
+  {
     fprintf(stderr, "Error Detected!\n"); 
     return 1;
   }
